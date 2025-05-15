3daa022829f71761f40f330bc911102afc0388d2
Maryam <kara0243@algonquinlive.com>
2025-05-15 11:03:03 -0400

    index and js created

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+//console.log will output a message in the dveloper console
+//two slashes starts a single line comment
+console.log("it worked"); //single line comment can go beside code
+
+/* 
+this is a multi-line comment
+starts with a / and * and close with a * and /
+*/
+
+// to declare a variable, use let or const
+const ourClassNum ="j207";
+let numStudents = 9; //with a number, you don't need quotes
+
+console.log(ourClassNum);
+console.log(numStudents);
+
+//if you declare a variable with let, you can change its value
+
+numStudents = 21;
+console.log(numStudents);
+//when the javascript has an error it stops running and whatever you add after it doesn't read it
+//if you declare a value with const, you can't change its value
+//declare means creating a variable, you can't have the same name, you can't recreat
+//let numStudents= 25; will give you an error
+
+ourClassNum = "c128";
+
+console.log(ourClassNum);
